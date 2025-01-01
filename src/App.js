9286e3eba136aae86755de29af701c8371d871ef
GLPCCL <35903427+GLPCCL@users.noreply.github.com>
2025-01-01 21:44:19 +0100

    Ajout du composant App principal

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SnakeGame from './components/SnakeGame';
+
+// Composant principal de l'application
+function App() {
+  return (
+    <div className="App">
+      <SnakeGame />
+    </div>
+  );
+}
+
+export default App;
